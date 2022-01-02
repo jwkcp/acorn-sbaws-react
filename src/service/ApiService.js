a030e3234b4dcc6e7f3b23592918bcf3fcf252a1
jwkcp <jaewoong.go@gmail.com>
2022-01-03 08:37:17 +0900

    엑세스 토큰 로컬스토리지 get/set 구현 완료

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -1,10 +1,18 @@
 import { API_BASE_URL } from "../app-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request) {
+    let headers = new Headers({
+        "Content-Type": "application/json",
+    });
+
+    const accessToken = localStorage.getItem(ACCESS_TOKEN);
+    if (accessToken && accessToken !== null) {
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
+
     var options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
-        }),
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -39,6 +47,8 @@ export function signin(userDTO) {
     return call("/auth/signin", "POST", userDTO).then((response) => {
         console.log("response: ", response);
         alert("Login Token: " + response.token);
+
+        localStorage.setItem(ACCESS_TOKEN, response.token);
 
         if (response.token) {
             window.location.href = "/";
