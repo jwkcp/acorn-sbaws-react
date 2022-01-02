e8cbfb3a6e762092a46ebc88c8f50bfb6947d6c0
jaewoong <jaewoong.go@gmail.com>
2022-01-02 23:38:56 +0900

    스프링 시큐리티를 이용한 암호화 적용 완료
    회원가입 시, 비밀번호 암호화
    로그인 시, 토큰 생성하여 리턴

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -13,14 +13,35 @@ export function call(api, method, request) {
         options.body = JSON.stringify(request);
     }
 
-    return fetch(options.url, options).then((response) =>
-        response.json().then((json) => {
-            console.log("Before response ok check");
-            if (!response.ok) {
-                return Promise.reject(json);
-            }
+    return fetch(options.url, options)
+        .then((response) =>
+            response.json().then((json) => {
+                console.log("Before response ok check");
+                if (!response.ok) {
+                    return Promise.reject(json);
+                }
 
-            return json;
-        })
-    );
+                return json;
+            })
+        )
+        .catch((error) => {
+            console.log("error occured");
+            console.log(error.status);
+            // if (error.status === 403) {
+            window.location.href = "/login";
+            // }
+
+            return Promise.reject(error);
+        });
+}
+
+export function signin(userDTO) {
+    return call("/auth/signin", "POST", userDTO).then((response) => {
+        console.log("response: ", response);
+        alert("Login Token: " + response.token);
+
+        if (response.token) {
+            window.location.href = "/";
+        }
+    });
 }
